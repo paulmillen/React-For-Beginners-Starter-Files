51787c1045f3bdabcf46332e275f1bb68ab07b8e
paulmillen <pwmillen@gmail.com>
2017-10-19 13:32:04 +0100

    Fish submit button disabled if fish unavailable

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     this.state = {
       fishes: {},
@@ -30,6 +31,12 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -38,7 +45,7 @@ class App extends React.Component {
           <ul className="list-of-fish">
             {
               Object.keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
